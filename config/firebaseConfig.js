7c67a7357b5db809c5b3739e0454336c746a2056
mac <mac@macs-MacBook-Air.local>
2024-10-31 04:16:26 +0700

    Add serviceAccountKey.json and update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,14 +1,15 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
+// config/firebaseConfig.js
+const admin = require('firebase-admin');
+// Thay thế đường dẫn tới tệp serviceAccountKey.json bằng đường dẫn thực tế
+const serviceAccount = require('./serviceAccountKey.json');
+// Khởi tạo Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://brainupdate-fee4b-default-rtdb.firebaseio.com/",
-
+  databaseURL: 'https://brain-update-20a9b-default-rtdb.firebaseio.com' // Thay thế <your-project-id> bằng đúng project ID của bạn
 });
-
+// Sử dụng Firestore database (nếu bạn đang dùng Firestore)
 const db = admin.firestore();
-module.exports = { admin, db };
-
-
+// Nếu sử dụng Realtime Database, có thể thêm một dòng để xuất ra DB cho Realtime Database như sau:
+// const realTimeDb = admin.database();
+// Xuất biến db để có thể sử dụng trong các file khác
+module.exports = { db };
