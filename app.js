b3741c03cc332844f11ed95e6bb2b287ce3fb15f
mac <mac@macs-MacBook-Air.local>
2024-10-31 04:29:33 +0700

    edit 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const quizRoutes = require('./routes/quizRoutes');
-const flashCardRoutes = require('./routes/flashCardRoutes'); // Sửa lại từ "flashCradRoutes" thành "flashCardRoutes"
+const flashCardRoutes = require('./routes/flashCardRoutes');
 const moduleRoutes = require('./routes/moduleRoutes');
 const directoryRoutes = require('./routes/directoryRoutes');
 
@@ -14,5 +14,10 @@ app.use('/api/flashcards', flashCardRoutes);
 app.use('/api/module', moduleRoutes);
 app.use('/api/directory', directoryRoutes);
 
-// Thay vì `app.listen`, xuất app như một module
+// Sử dụng `process.env.PORT` thay vì cổng cố định
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
