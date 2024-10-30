7b54d2944497158f9a1f41243c65e6032d0c131d
mac <mac@macs-MacBook-Air.local>
2024-10-31 05:17:31 +0700

    edit -6

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/module', moduleRoutes);
 app.use('/api/directory', directoryRoutes);
 
 // Sử dụng `process.env.PORT` thay vì cổng cố định
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
